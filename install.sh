b095101fc29ea5bcc2daf029e47a668ecf544065
Junian <git@junian.net>
2017-08-16 17:07:30 +0700

    added install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing dotfiles to home";
+echo "Creating symlinks of dotfiles";
+
+
+
+echo "Finished symlinking all dotfiles. Restart your shell terminal to take effect.";
