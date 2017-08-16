0bb752c22e2b9dd425af58a9b6d48df1de8d57ed
Junian <git@junian.net>
2017-08-17 04:25:19 +0700

    efficient install

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 echo "Installing dotfiles to home";
 echo "Creating symlinks of dotfiles";
 
-ln -sv $PWD/.functions $HOME
-ln -sv $PWD/.vimrc $HOME
-ln -sv $PWD/.aliases $HOME
-ln -sv $PWD/.bash_profile $HOME
+for file in .{functions,aliases,vimrc,bash_profile}; do
+    [ -r "$PWD/$file" ] && [ -f "$PWD/$file" ] && rm "$HOME/$file" && ln -sv "$PWD/$file" $HOME;
+done;
+unset file;
 
 echo "Finished symlinking all dotfiles. Restart your shell terminal to take effect.";
