89e800e0468a23cabeca83b902be4a64961b4593
Junian <git@junian.net>
2017-08-16 17:12:45 +0700

    can install .vimrc

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo "Installing dotfiles to home";
 echo "Creating symlinks of dotfiles";
 
 ln -sv $PWD/.functions $HOME
+ln -sv $PWD/.vimrc $HOME
 
 echo "Finished symlinking all dotfiles. Restart your shell terminal to take effect.";
