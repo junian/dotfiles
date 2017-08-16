67bca808cb7f9e677a2338f8cfb3e940c3689f20
Junian <git@junian.net>
2017-08-16 17:16:51 +0700

    can install .bash_profile

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ echo "Creating symlinks of dotfiles";
 ln -sv $PWD/.functions $HOME
 ln -sv $PWD/.vimrc $HOME
 ln -sv $PWD/.aliases $HOME
+ln -sv $PWD/.bash_profile $HOME
 
 echo "Finished symlinking all dotfiles. Restart your shell terminal to take effect.";
